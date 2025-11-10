f055b29d666e591073aa52ce9c5948448065b0c0
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-10 19:40:37 +0900

    test(string): 단순한 생각

--- src/test/java/com/wooa/StringTest.java
+++ src/test/java/com/wooa/StringTest.java
@@ -3,6 +3,7 @@ package com.wooa;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class StringTest {
 
@@ -20,6 +21,15 @@ class StringTest {
     void substringTest() {
         String testString = "(1,2)";
         assertThat(testString.substring(1,testString.length()-1)).isEqualTo("1,2");
+    }
+
+    @Test
+    @CsvSource({"a", "b", "c"})
+    void chatAtTest(char target) {
+        String testString = "abc";
+        assertThat(testString.charAt(0)).isEqualTo(target);
+        assertThat(testString.charAt(1)).isEqualTo(target);
+        assertThat(testString.charAt(2)).isEqualTo(target);
     }
 
 }
