ca378a3bc2a3800597a45745feb8e8436d79a5dc
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-10 20:24:21 +0900

    test(set): setUp - BeforeEach

---
+++ src/test/java/com/wooa/SetTest.java
@@ -0,0 +1,23 @@
+package com.wooa;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+}
